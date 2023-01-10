f9073e9055c1ef708ec57d031881e39cc4474648
Alexey <advreid@yandex.ru>
2023-01-10 14:05:38 +1000

    change popup + fix

--- index.js
+++ index.js
@@ -6,24 +6,21 @@ let profileName = document.querySelector('.profile__name');
 let profileBio = document.querySelector('.profile__bio');
 let inputName = document.querySelector('.popup__input_name');
 let inputBio = document.querySelector('.popup__input_bio');
+let formElement = document.querySelector('.popup__form');
 
 profileEdit.addEventListener('click', function () {
-
     popup.classList.add('popup_opened');
-
+    inputName.value = profileName.textContent;
+    inputBio.value = profileBio.textContent;
 });
 
 popupClose.addEventListener('click', function () {
-
     popup.classList.remove('popup_opened');
-
 });
 
-let formElement = document.querySelector('.popup__form');
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector('.popup__input_name');
-    let jobInput = document.querySelector('.popup__input_bio');
     profileName.textContent = inputName.value;
     profileBio.textContent = inputBio.value;
     popup.classList.remove('popup_opened');
