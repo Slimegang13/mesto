d2656d8d4f8598396ad36bb1ff28ea50381d16e6
Alexey <advreid@yandex.ru>
2023-01-11 21:30:58 +1000

    new version

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ let inputName = document.querySelector('.popup__input_name');
 let inputBio = document.querySelector('.popup__input_bio');
 let formElement = document.querySelector('.popup__form');
 
+
+// Открытие Popup 
 profileEdit.addEventListener('click', function () {
     popup.classList.add('popup_opened');
     inputName.value = profileName.textContent;
     inputBio.value = profileBio.textContent;
 });
 
+// Закрытие Popup
 popupClose.addEventListener('click', function () {
     popup.classList.remove('popup_opened');
 });
 
-
+// Отправка формы в попапе
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileBio.textContent = inputBio.value;
     popup.classList.remove('popup_opened');
 }
-
 formElement.addEventListener('submit', formSubmitHandler);
 
 
